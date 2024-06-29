2a34d31f91cb34cf015aa45216e2d3522038f1e5
AlbertChoe <albert.choe73@gmail.com>
2024-06-30 00:34:48 +0700

    add speed insight vercell

--- salon_web/app/layout.tsx
+++ salon_web/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientLayout from "./clientLayout";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
